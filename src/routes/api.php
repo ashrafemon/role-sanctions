0ad2e1a25215267e000cb3b29b8dbf3cde0d3128
Ashraf Emon <ashraf.emon143@gmail.com>
2023-03-21 11:31:50 +0600

    permission route bug fix

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->prefix('api/v1')->group(function () {
-    Route::apiResource('permissions', \Leafwrap\RoleSanctions\Http\Controllers\PermissionController::class)->except(['create', 'edit']);
+    Route::apiResource('permissions', \Leafwrap\RoleSanctions\Http\Controllers\PermissionController::class)->only(['index']);
     Route::apiResource('roles', \Leafwrap\RoleSanctions\Http\Controllers\RoleController::class)->except(['create', 'edit']);
 });
